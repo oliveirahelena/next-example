ea9a3fdf7a0c7b2b671f912f53fef3acf9307e97
Helena Oliveira <helena.oliveira@casaldev.com.br>
2021-03-21 09:07:16 -0300

    add more organisms

---
+++ src/components/organisms/GridText/index.tsx
@@ -0,0 +1,47 @@
+import * as Styled from './styles';
+import { SectionBackground } from '../../molecules/SectionBackground';
+import { Heading } from '../../atoms/Heading';
+import { TextComponent } from '../../atoms/TextComponent';
+
+export type GridTextElementProps = {
+  title: string;
+  description: string;
+};
+
+export type GridTextProps = {
+  background?: boolean;
+  title: string;
+  description: string;
+  grid: GridTextElementProps[];
+  sectionId?: string;
+  component?: string;
+};
+
+export const GridText = ({
+  title,
+  description,
+  grid,
+  background = false,
+  sectionId = '',
+}: GridTextProps) => {
+  return (
+    <SectionBackground background={background} sectionId={sectionId}>
+      <Styled.Container>
+        <Heading size="huge" uppercase colorDark={!background} as="h2">
+          {title}
+        </Heading>
+        <TextComponent>{description}</TextComponent>
+        <Styled.Grid>
+          {grid.map((el) => (
+            <Styled.GridElement key={el.title}>
+              <Heading size="medium" colorDark={!background} as="h3">
+                {el.title}
+              </Heading>
+              <TextComponent>{el.description}</TextComponent>
+            </Styled.GridElement>
+          ))}
+        </Styled.Grid>
+      </Styled.Container>
+    </SectionBackground>
+  );
+};
